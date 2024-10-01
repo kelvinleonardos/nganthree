75afa9c2c601087319c8b324ec5a55f8e8eb2151
kelvinleonardos <kelvinlsianipar@gmail.com>
2024-10-01 18:02:14 +0800

    fix: fix some bug

--- backend/nganthree-backend/src/server.js
+++ backend/nganthree-backend/src/server.js
@@ -5,6 +5,8 @@ import queueRoutes from './routes/queueRoutes.js';
 import statusRoutes from './routes/statusRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(json());
